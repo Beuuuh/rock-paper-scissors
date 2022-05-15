33e81027171ab67fd74da7d66f07633b599259f0
Beuuuh <bellmarques8768@hotmail.com>
2022-05-14 23:32:46 -0300

    tiebreaker added, project completed

--- javascript.js
+++ javascript.js
@@ -11,22 +11,33 @@ function computerPlay() {
 
 let computerSelection
 let playerSelection
+let playerWins = 0
+let computerWins = 0
+let tiebreaker = 0
 
 function playRound(playerSelection, computerSelection) {
+    // checks every possibility
     if(playerSelection == "paper" && computerSelection == "scissors") {
         console.log(`You have put ${playerSelection} and computer have put ${computerSelection}... computer wins!`)
+        computerWins++
     } else if(playerSelection == "rock" && computerSelection == "paper") {
         console.log(`You have put ${playerSelection} and computer have put ${computerSelection}... computer wins!`)
+        computerWins++
     } else if(playerSelection == "scissors" && computerSelection == "rock") {
         console.log(`You have put ${playerSelection} and computer have put ${computerSelection}... computer wins!`)
+        computerWins++
     } if(playerSelection == "paper" && computerSelection == "rock") {
         console.log(`You have put ${playerSelection} and computer have put ${computerSelection}... you won!`)
+        playerWins++
     } else if(playerSelection == "rock" && computerSelection == "scissors") {
         console.log(`You have put ${playerSelection} and computer have put ${computerSelection}... you won!`)
+        playerWins++
     } else if(playerSelection == "scissors" && computerSelection == "paper") {
         console.log(`You have put ${playerSelection} and computer have put ${computerSelection}... you won!`)
+        playerWins++
     } else if(playerSelection == computerSelection) {
         console.log("It's a draw!")
+        tiebreaker++
     }
 }
 
@@ -35,6 +46,17 @@ function game() {
         computerSelection = computerPlay()
         playerSelection = prompt("Try it out").toLowerCase()
         playRound(playerSelection, computerSelection)
+        if(i == 5 && playerWins > computerWins) {
+            console.log("YOU WON THE GAME!")
+        } else if(i == 5 && playerWins < computerWins) {
+            console.log("You lost the game!")
+        } else {
+            while(playerWins == computerWins) {
+                computerSelection = computerPlay()
+                playerSelection = prompt("Try it out").toLowerCase()
+                playRound(playerSelection, computerSelection)
+            }
+        }
     }
 }
 
